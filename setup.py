decdd99427a17f5ccde4636f36e03b467cce618d
Leo Basov <leo.basov@dlr.de>
2024-11-08 16:07:46 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+
+setup( 
+	name='landau_teller', 
+	version='0.1.0', 
+	description='Evaluates 0D Ladau-Teller like relaxation of internal degrees of freedom for mixtures with polyatomic species', 
+	author='Leo Basov', 
+	packages=['landau_teller'], 
+	install_requires=[ 
+		'numpy',
+	], 
+) 
