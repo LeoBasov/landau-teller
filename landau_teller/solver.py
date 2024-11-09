3a33527538c7a84032fb333648a231d4d53199f3
Leo Basov <basov.leo@gmail.com>
2024-11-09 11:58:13 +0100

    implemented solver functions

--- landau_teller/solver.py
+++ landau_teller/solver.py
@@ -1,5 +1,27 @@
 import numpy as np
+from numbers import Number
 from .constants import kb
 
 def calc_coll_freq(gas, nrho, temp):
     return 4.0 * gas.vhs.dref**2 * nrho * np.sqrt(np.pi * kb * gas.vhs.Tref / gas.mass) * (temp/gas.vhs.Tref)**(1.0 - gas.vhs.omega)
+
+def calc_ekin(gas, temp):
+    return 1.5 * kb * temp
+
+def calc_erot(gas, temp):
+    return 0.5 * gas.dof_rot * kb * temp
+
+def calc_evib(gas, temp):
+    evib = np.zeros(len(gas.vibmodes))
+
+    for i in range(len(evib)):
+        if isinstance(temp, Number):
+            frac = gas.vibmodes[i].theta/temp
+            evib[i] = gas.vibmodes[i].degen * 0.5 * kb * frac / (np.exp(frac) - 1.0) * temp
+        elif len(temp) == len(gas.vibmodes):
+            frac = gas.vibmodes[i].theta/temp[i]
+            evib[i] = gas.vibmodes[i].degen * 0.5 * kb * frac / (np.exp(frac) - 1.0) * temp[i]
+        else:
+            raise Exception("wrong format")
+
+    return evib
--- tests/unit/solver_test.py
+++ tests/unit/solver_test.py
@@ -16,3 +16,14 @@ class TestSolver(unittest.TestCase):
         nu = so.calc_coll_freq(co2, nrho, temp)
 
         self.assertEqual(1.9299072751158094e-05, 1/nu)
+
+    def test_calc_evib(self):
+        file_name = "data/CO2.json"
+        co2 = rd.read_species(file_name)
+        temp = 300
+        temps = [300, 400, 500]
+
+        evibs1 = so.calc_evib(co2, temp)
+        evibs2 = so.calc_evib(co2, temps)
+
+        self.assertEqual(evibs1[0], evibs2[0])
