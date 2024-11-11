82caf1e2c8cbb1b9ce5165d009ab3cecb7325efa
Leo Basov <leo.basov@dlr.de>
2024-11-11 11:31:04 +0100

    updated writer

--- landau_teller/writer.py
+++ landau_teller/writer.py
@@ -6,7 +6,7 @@ def write_solution(solution, file_name, format):
     
 def write_solution_csv(solution, file_name):
     with open(file_name + "_temp.csv", "w") as file:
-        file.write("#t, temp, trot")
+        file.write("t, temp, trot")
 
         for i in range(solution.temperatures[0].size - 2):
             file.write(", tvib_" + str(i + 1))
@@ -22,7 +22,7 @@ def write_solution_csv(solution, file_name):
             file.write("\n")
 
     with open(file_name + "_energy.csv", "w") as file:
-        file.write("#t, ekin, erot")
+        file.write("t, ekin, erot")
 
         for i in range(solution.temperatures[0].size - 2):
             file.write(", evib_" + str(i + 1))
